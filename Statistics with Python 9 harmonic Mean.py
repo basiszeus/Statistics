b150c47abd3dfbdf92a869ce416fbc381cb5bc92
walid soula <84076926+basiszeus@users.noreply.github.com>
2022-06-08 12:02:31 +0100

    Add files via upload

---
+++ Statistics with Python 9 harmonic Mean.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+import scipy 
+from scipy.stats import hmean
+
+
+# In[3]:
+
+
+X = (10,14)
+
+
+# In[5]:
+
+
+hmean(X)
+
+
+# In[ ]:
+
+
+
+
